a40417f81d5cb525b0d3afb5215944aeffc286d5
d3bes <abdo.mohamed6319@gmail.com>
2024-07-03 23:12:54 +0300

    add basic DbContext and Dbsets<>

---
+++ src/project.EF/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using project.Core.Models;
+
+
+namespace project.EF
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+    }
+}
