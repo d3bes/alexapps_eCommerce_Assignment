08bae7e0f9066276be69e4d6a97691ff888e2993
d3bes <abdo.mohamed6319@gmail.com>
2024-07-04 02:08:41 +0300

    add merchant role bilerplate

--- src/project.Core/Consts/Boilerplate.cs
+++ src/project.Core/Consts/Boilerplate.cs
@@ -10,7 +10,9 @@ namespace project.Core.Consts
         public const string boilerplateAdminId = "92c8a8b2-647f-456f-a787-20e396c8bc84";
         public const string boilerplateAdminRoleId = "72ede098-49aa-4056-8912-63b605e52740";
         public const string boilerplateUserRoleId = "50d0a48f-d3a0-4598-9806-c00f9b18fd68";
+        public const string boilerplateMerchantRoleId = "f63cc1cd-bfff-428a-ab6c-24eec6e9a5d5";
 
+         
         public const string AdminPasswordHash = "AQAAAAIAAYagAAAAEB9Egl04rGcrKfVSsFecpExW/EZdaGCs4hZkzeecpd6mP9ntxAu4fOEJmDGfQQZpgQ==";
 
 
