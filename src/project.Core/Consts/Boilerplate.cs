8ffee52afeff867cdc87885e0eacb385cc4a3aa9
d3bes <abdo.mohamed6319@gmail.com>
2024-07-04 00:54:10 +0300

    add biolerplate consts

---
+++ src/project.Core/Consts/Boilerplate.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.Core.Consts
+{
+    public class Boilerplate
+    {
+        public const string boilerplateAdminId = "92c8a8b2-647f-456f-a787-20e396c8bc84";
+        public const string boilerplateAdminRoleId = "72ede098-49aa-4056-8912-63b605e52740";
+        public const string boilerplateUserRoleId = "50d0a48f-d3a0-4598-9806-c00f9b18fd68";
+
+        public const string AdminPasswordHash = "AQAAAAIAAYagAAAAEB9Egl04rGcrKfVSsFecpExW/EZdaGCs4hZkzeecpd6mP9ntxAu4fOEJmDGfQQZpgQ==";
+
+
+
+    }
+}
