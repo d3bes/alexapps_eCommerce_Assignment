6e14459ee649aeddc5fbe638dc395584ef655c81
d3bes <abdo.mohamed6319@gmail.com>
2024-07-04 00:54:30 +0300

    add Role consts

---
+++ src/project.Core/Consts/Role.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.Core.Consts
+{
+    public class Role
+    {
+         public const string admin = "Admin";
+         public const string user = "User";
+
+    }
+}
