3cc79442b38c37a4900988b850b9e09b73e8237e
d3bes <abdo.mohamed6319@gmail.com>
2024-07-03 23:14:52 +0300

    add IdentityUser and DbConnection

--- src/project.Api/Program.cs
+++ src/project.Api/Program.cs
@@ -1,9 +1,24 @@
+using project.Core.Models;
+using project.EF;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("localServer"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
