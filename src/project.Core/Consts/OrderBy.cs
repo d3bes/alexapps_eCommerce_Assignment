017d8adb05414317c9428d5f01889011d558d00e
d3bes <abdo.mohamed6319@gmail.com>
2024-07-04 15:50:15 +0300

    add order by const

---
+++ src/project.Core/Consts/OrderBy.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.Core.Consts
+{
+    public class OrderBy
+    {
+        public const string Ascending = "ASC";
+        public const string Descending = "DESC";
+    }
+}
