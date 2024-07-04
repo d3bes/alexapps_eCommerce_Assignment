c7729279f0001f5dc0a1cb6021ff81bf4b009332
d3bes <abdo.mohamed6319@gmail.com>
2024-07-04 15:59:09 +0300

    update tables

--- src/project.EF/ApplicationDbContext.cs
+++ src/project.EF/ApplicationDbContext.cs
@@ -9,14 +9,29 @@ using project.Core.Models;
 
 namespace project.EF
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
-        }
+        }      
+        
+        //   public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Merchant> merchants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<User>(entity => entity.ToTable("Users"));
+            builder.Entity<Merchant>(entity => entity.ToTable("Merchants"));
+            builder.Entity<Cart>(entity => entity.ToTable("Carts"));
+            builder.Entity<CartItem>(entity => entity.ToTable("CartItems"));
+            builder.Entity<Product>(entity => entity.ToTable("Products"));
+
+        }
     }
 }
