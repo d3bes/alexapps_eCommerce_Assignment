5c4f5646275214c1f2cdb8df799a3ced6fcc846e
d3bes <abdo.mohamed6319@gmail.com>
2024-07-04 15:56:06 +0300

    add FullName  property

--- src/project.Core/Models/User.cs
+++ src/project.Core/Models/User.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace project.Core.Models
 {
-    public class User : ApplicationUser
+    public class User : IdentityUser
     {
-        
+       public string FullName { get; set; }
+
     }
 }
