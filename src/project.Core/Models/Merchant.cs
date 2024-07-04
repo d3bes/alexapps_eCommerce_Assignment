ab78e3da80db7d74705d15665dfdd550acf261ce
d3bes <abdo.mohamed6319@gmail.com>
2024-07-04 15:55:49 +0300

    add userId property

--- src/project.Core/Models/Merchant.cs
+++ src/project.Core/Models/Merchant.cs
@@ -2,17 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
- using Microsoft.AspNetCore.Identity;
- using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace project.Core.Models
 {
-    public class Merchant : ApplicationUser
+    public class Merchant 
     {
-     public string StoreName { get; set; }
-    public bool IsVatIncluded { get; set; }
-    public decimal ShippingCost { get; set; }
-    public decimal VatPercentage { get; set; }
-     public ICollection<Product> Products { get; set; }
+        public int Id { get; set; }
+        public string StoreName { get; set; }
+        public bool IsVatIncluded { get; set; }
+        public decimal ShippingCost { get; set; }
+        public decimal VatPercentage { get; set; }
+        
+        public string UserId { get; set; }
+        public User user { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
