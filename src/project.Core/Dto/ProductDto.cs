9a035c30a06dbd40fe5ac09d9730312b2891486b
d3bes <abdo.mohamed6319@gmail.com>
2024-07-05 22:41:00 +0300

    add [Required]

--- src/project.Core/Dto/ProductDto.cs
+++ src/project.Core/Dto/ProductDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,18 @@ namespace project.Core.Dto
     public class ProductDto
     {
         public int Id { get; set; }
+        
+        [Required]
         public string NameEn { get; set; }
+        [Required]
         public string NameAr { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
         public string DescriptionEn { get; set; }
+        [Required]
         public string DescriptionAr { get; set; }
+        [Required]
         public decimal Price { get; set; }
     }
 }
