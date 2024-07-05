e0d6a07399b4a2348ab0b799a2dec480424b0f2b
d3bes <abdo.mohamed6319@gmail.com>
2024-07-05 17:22:43 +0300

    update

--- src/project.Core/Dto/RegisterMerchantDto.cs
+++ src/project.Core/Dto/RegisterMerchantDto.cs
@@ -9,15 +9,6 @@ namespace project.Core.Dto
     public class RegisterMerchantDto
     {
         public string Email { get; set; }
-
-        [Required]
-        public string FullName { get; set; }
-
-
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
         [Required]
         public string PhoneNumber { get; set; }
 
@@ -31,17 +22,6 @@ namespace project.Core.Dto
 
         public decimal? VatPercentage { get; set; }
 
-          public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            var validationResults = new List<ValidationResult>();
-
-            if (IsVatIncluded && !VatPercentage.HasValue)
-            {
-                validationResults.Add(new ValidationResult("VatPercentage is required when IsVatIncluded is true.", new[] { nameof(VatPercentage) }));
-            }
-
-            return validationResults;
-        }
     }
 
 }
