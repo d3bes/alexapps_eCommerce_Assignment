4d68770c31c8e9412c564d1781a7e17c640d5325
d3bes <abdo.mohamed6319@gmail.com>
2024-07-05 22:47:18 +0300

    add ToUserDto

--- src/project.Api/Extensions/AccountExtensions.cs
+++ src/project.Api/Extensions/AccountExtensions.cs
@@ -10,13 +10,33 @@ namespace project.Api.Extensions
 {
     public static class AccountExtensions
     {
+        public static string ToUserName(this string fullName)
+        {
+            var rand = new Random();
+            var random = rand.Next(1,999).ToString();
+           var full_Name= fullName.Replace(" ", "_").ToLower();
+            string UserName = $"{full_Name}_{random}";
+            return UserName;
+        }
         public static User ToUser(this RegisterDto registerDto)
         {
             return new User(){
-            Email = registerDto.
-
-
+            Email = registerDto.Email,
+            FullName = registerDto.FullName,
+            UserName = registerDto.FullName.ToUserName()
             };
+        }
+
+        public static UserDto ToUserDto(this User user)
+        {
+            return new UserDto(){
+                Email = user.Email,
+                FullName = user.FullName,
+                Id = user.Id,
+                phoneNumber = user.PhoneNumber,
+                UserName = user.FullName
+            };
+
         }
     }
 }
