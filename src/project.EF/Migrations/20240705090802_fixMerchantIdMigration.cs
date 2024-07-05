924302d7dacecaba79e02a8463a475a82198695d
d3bes <abdo.mohamed6319@gmail.com>
2024-07-05 12:10:13 +0300

    fix MerchantId duplicate

---
+++ src/project.EF/Migrations/20240705090802_fixMerchantIdMigration.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixMerchantIdMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Merchants_MerchantId1",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_MerchantId1",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "MerchantId1",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MerchantId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VatPercentage",
+                table: "Merchants",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_MerchantId",
+                table: "Products",
+                column: "MerchantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Merchants_MerchantId",
+                table: "Products",
+                column: "MerchantId",
+                principalTable: "Merchants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Merchants_MerchantId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_MerchantId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MerchantId",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MerchantId1",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VatPercentage",
+                table: "Merchants",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_MerchantId1",
+                table: "Products",
+                column: "MerchantId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Merchants_MerchantId1",
+                table: "Products",
+                column: "MerchantId1",
+                principalTable: "Merchants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
