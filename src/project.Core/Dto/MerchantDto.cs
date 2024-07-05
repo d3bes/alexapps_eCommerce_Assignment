e94c4927565a3a310acc75b543d30554bce25145
d3bes <abdo.mohamed6319@gmail.com>
2024-07-05 17:21:12 +0300

    init

---
+++ src/project.Core/Dto/MerchantDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using project.Core.Models;
+
+namespace project.Core.Dto
+{
+    public class MerchantDto
+    {
+        public int Id { get; set; }
+        public string StoreName { get; set; }
+        public bool IsVatIncluded { get; set; }
+
+        public decimal ShippingCost { get; set; }
+        public decimal? VatPercentage { get; set; }
+
+        public UserDto user { get; set; }
+         public List<MerchantProductDto> products { get; set; }
+
+
+
+    }
+}
---
+++ src/project.Core/Dto/ProductDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.Core.Dto
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string NameEn { get; set; }
+        public string NameAr { get; set; }
+        public string DescriptionEn { get; set; }
+        public string DescriptionAr { get; set; }
+        public decimal Price { get; set; }
+    }
+}
