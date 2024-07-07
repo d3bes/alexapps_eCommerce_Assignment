55ebd2c469fdb5a88d062b86ce90cfe4dd5af6f2
d3bes <abdo.mohamed6319@gmail.com>
2024-07-07 18:28:02 +0300

    init

---
+++ src/project.Core/Dto/ToggleIsVatIncludedDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.Core.Dto
+{
+    public class ToggleIsVatIncludedDto
+    {
+        public decimal VatPercentage { get; set; }                         
+    }
+}
